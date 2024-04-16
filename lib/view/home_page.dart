9b1b6225699be9e3efb62810ecc3b70d8659d18b
MidhileshRaj <midhileshraj01@gmail.com>
2024-04-16 12:47:19 +0530

    dependancies added

---
+++ lib/view/home_page.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final PlaylistProvider playlistProvider;
+
+  @override
+  void initState() {
+    super.initState();
+    playlistProvider = Provider.of<PlaylistProvider>(context, listen: false);
+  }
+
+  void goToSong(int songIndex) {
+    playlistProvider.currentSongIndex = songIndex;
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const SongPage(),
+        ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      appBar: AppBar(title: const Text('P L A Y L I S T')),
+      drawer: const MyDrawer(),
+      body: Consumer<PlaylistProvider>(
+        builder: (context, value, child) {
+          final List<Songs> playlist = value.playlist;
+
+          return ListView.builder(
+            itemCount: playlist.length,
+            itemBuilder: (context, index) {
+              final Songs song = playlist[index];
+
+              return ListTile(
+                title: Text(
+                  song.songName,
+                  style: const TextStyle(fontSize: 20),
+                ),
+                subtitle: Text(song.artistName),
+                leading: Image.asset(song.imagePath),
+                contentPadding: const EdgeInsets.all(10),
+                onTap: () {
+                  goToSong(index);
+                },
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
